13b1b47eb2577edd3efaafa833aae9eaf81443dd
Valerie Wireko Osei <valeriew@SML391TO.local>
2021-08-18 23:03:10 -0400

    working on basic problems

---
+++ practice1.js
@@ -0,0 +1,28 @@
+// Return the sum of two numbers 
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(add(2, 3))
+
+// Convert minutes to seconds 
+const minutesToSeconds = (sec) => {
+    return sec * 60
+}
+
+console.log(minutesToSeconds(6))
+
+// Return the Next Number from the Integer Passed
+const nextNumber = (num) => {
+    return num + 1
+}
+
+console.log(nextNumber(6))
+
+// Area of a Triangle
+const areaOfTriangle = (base, height) => {
+    const area = (base * height) / 2
+    return area
+}
+
+console.log(areaOfTriangle(6, 4))
