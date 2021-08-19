d175a4b7457286dc28d8666219c79fbd0fa8c264
Valerie Wireko Osei <valeriew@SML391TO.local>
2021-08-19 00:07:27 -0400

    working on more basic problems

--- practice1.js
+++ practice1.js
@@ -26,3 +26,45 @@ const areaOfTriangle = (base, height) => {
 }
 
 console.log(areaOfTriangle(6, 4))
+
+// Convert Age to Days
+const ageToDays = (age) => {
+    return age * 365
+}
+
+console.log(ageToDays(4))
+
+// Return the First Element in an Array
+const firstElement = (arr) => {
+    return arr[0]
+}
+
+console.log(firstElement([13, 5, 6, 23, 4]))
+
+// Find the Perimeter of a Rectangle
+const perimeterOfRectangle = (length, width) => {
+    const perimeter = (length * 2) + (width * 2)
+    return perimeter
+}
+
+console.log(perimeterOfRectangle(3, 6))
+
+// Return Something to Me! 
+const returnSomething = (message) => {
+    return "something " + message
+}
+
+console.log(returnSomething("is missing"))
+
+// Is the Number Less than or Equal to Zero? 
+const lessThanEqualZero = (num) => {
+    if(num > 0) {
+        return true
+    } else if(num < 0) {
+        return false
+    } else {
+        return "Number is zero"
+    }
+}
+
+console.log(lessThanEqualZero(0))
